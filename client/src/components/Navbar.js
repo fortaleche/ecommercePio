93147060628aa0bdaab5baeddd29f7b467750e63
Dilier Esteban Fortaleche Lopez <dilierestebanfortalechelopez@gmail.com>
2024-10-30 17:56:27 -0500

    actu1.5

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
     const navigate = useNavigate();
     const handleLogout = () => {
-        localStorage.removeItem('token');
+        localStorage.removeItem("token");
         navigate("/login");
     };
     return (
-        <nav>
-            Hola Mundo
+        <nav className="navbar"> 
+            <Link to="/">Home</Link>
+            {localStorage.getItem("token") ? (
+              <>
+                <link to="/admin">admin</link>
+                <button onClick={handleLogout}>Logout</button>
+              </>
+            ): (
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </> 
+            )
+            }
         </nav>
     )
     }
 
-export default Navbar;
+export default Navbar
